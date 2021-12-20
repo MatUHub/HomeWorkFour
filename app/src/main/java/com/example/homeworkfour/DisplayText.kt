f955f2486c278bea32c3217fcab5cfd43e2b34a4
MatU <radalert@mail.ru>
2021-12-21 00:00:36 +0300

    Преобразование проекта с Java в Kotlin

--- app/src/main/java/com/example/homeworkfour/DisplayText.kt
+++ app/src/main/java/com/example/homeworkfour/DisplayText.kt
@@ -1,47 +1,36 @@
-package com.example.homeworkfour;
+package com.example.homeworkfour
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcelable
+import android.os.Parcel
+import android.os.Parcelable.Creator
+import com.example.homeworkfour.DisplayText
 
-public class DisplayText implements Parcelable {
+class DisplayText : Parcelable {
+    var text: String? = ""
 
-    private String text = "";
-
-    protected DisplayText(Parcel in) {
-        text = in.readString();
+    protected constructor(`in`: Parcel) {
+        text = `in`.readString()
     }
 
-    public static final Creator<DisplayText> CREATOR = new Creator<DisplayText>() {
-        @Override
-        public DisplayText createFromParcel(Parcel in) {
-            return new DisplayText(in);
+    constructor() {}
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(parcel: Parcel, i: Int) {
+        parcel.writeString(text)
+    }
+
+    companion object {
+        val CREATOR: Creator<DisplayText?> = object : Creator<DisplayText?> {
+            override fun createFromParcel(`in`: Parcel): DisplayText? {
+                return DisplayText(`in`)
+            }
+
+            override fun newArray(size: Int): Array<DisplayText?> {
+                return arrayOfNulls(size)
+            }
         }
-
-        @Override
-        public DisplayText[] newArray(int size) {
-            return new DisplayText[size];
-        }
-    };
-
-    public DisplayText() {
-
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(text);
     }
 }
--- app/src/main/java/com/example/homeworkfour/MainActivity.kt
+++ app/src/main/java/com/example/homeworkfour/MainActivity.kt
@@ -1,179 +1,169 @@
-package com.example.homeworkfour;
+package com.example.homeworkfour
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import com.example.homeworkfour.DisplayText
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.homeworkfour.R
+import com.example.homeworkfour.MainActivity
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    static String TAG = "displayText";
-
-    TextView textView;
-
-    Button buttonOne;
-    Button buttonTwo;
-    Button buttonThree;
-    Button buttonFour;
-    Button buttonFive;
-    Button buttonSix;
-    Button buttonSeven;
-    Button buttonEight;
-    Button buttonNine;
-    Button buttonZero;
-    Button buttonPlus;
-    Button buttonSubtract;
-    Button buttonMultiply;
-    Button buttonDivision;
-    Button buttonComma;
-    Button buttonDot;
-    Button buttonEquals;
-
-    com.example.homeworkfour.DisplayText displayText;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        initView();
-        initListener();
-        displayText = new com.example.homeworkfour.DisplayText();
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var textView: TextView? = null
+    var buttonOne: Button? = null
+    var buttonTwo: Button? = null
+    var buttonThree: Button? = null
+    var buttonFour: Button? = null
+    var buttonFive: Button? = null
+    var buttonSix: Button? = null
+    var buttonSeven: Button? = null
+    var buttonEight: Button? = null
+    var buttonNine: Button? = null
+    var buttonZero: Button? = null
+    var buttonPlus: Button? = null
+    var buttonSubtract: Button? = null
+    var buttonMultiply: Button? = null
+    var buttonDivision: Button? = null
+    var buttonComma: Button? = null
+    var buttonDot: Button? = null
+    var buttonEquals: Button? = null
+    var displayText: DisplayText? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+        initListener()
+        displayText = DisplayText()
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putParcelable(TAG, displayText);
+    public override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(TAG, displayText)
     }
 
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        displayText = savedInstanceState.getParcelable(TAG);
-        textView.setText(displayText.getText());
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        displayText = savedInstanceState.getParcelable(TAG)
+        textView!!.text = displayText!!.text
     }
 
-    private void initListener() {
-        buttonOne.setOnClickListener(this);
-        buttonTwo.setOnClickListener(this);
-        buttonThree.setOnClickListener(this);
-        buttonFour.setOnClickListener(this);
-        buttonFive.setOnClickListener(this);
-        buttonSix.setOnClickListener(this);
-        buttonSeven.setOnClickListener(this);
-        buttonEight.setOnClickListener(this);
-        buttonNine.setOnClickListener(this);
-        buttonZero.setOnClickListener(this);
-        buttonPlus.setOnClickListener(this);
-        buttonSubtract.setOnClickListener(this);
-        buttonMultiply.setOnClickListener(this);
-        buttonDivision.setOnClickListener(this);
-        buttonComma.setOnClickListener(this);
-        buttonDot.setOnClickListener(this);
-        buttonEquals.setOnClickListener(this);
+    private fun initListener() {
+        buttonOne!!.setOnClickListener(this)
+        buttonTwo!!.setOnClickListener(this)
+        buttonThree!!.setOnClickListener(this)
+        buttonFour!!.setOnClickListener(this)
+        buttonFive!!.setOnClickListener(this)
+        buttonSix!!.setOnClickListener(this)
+        buttonSeven!!.setOnClickListener(this)
+        buttonEight!!.setOnClickListener(this)
+        buttonNine!!.setOnClickListener(this)
+        buttonZero!!.setOnClickListener(this)
+        buttonPlus!!.setOnClickListener(this)
+        buttonSubtract!!.setOnClickListener(this)
+        buttonMultiply!!.setOnClickListener(this)
+        buttonDivision!!.setOnClickListener(this)
+        buttonComma!!.setOnClickListener(this)
+        buttonDot!!.setOnClickListener(this)
+        buttonEquals!!.setOnClickListener(this)
     }
 
-    private void initView() {
-        textView = findViewById(R.id.text_view);
-
-        buttonOne = findViewById(R.id.button_one);
-        buttonTwo = findViewById(R.id.button_two);
-        buttonThree = findViewById(R.id.button_three);
-        buttonFour = findViewById(R.id.button_four);
-        buttonFive = findViewById(R.id.button_five);
-        buttonSix = findViewById(R.id.button_six);
-        buttonSeven = findViewById(R.id.button_seven);
-        buttonEight = findViewById(R.id.button_eight);
-        buttonNine = findViewById(R.id.button_nine);
-        buttonZero = findViewById(R.id.button_zero);
-        buttonPlus = findViewById(R.id.button_plus);
-        buttonSubtract = findViewById(R.id.button_subtract);
-        buttonMultiply = findViewById(R.id.button_multiply);
-        buttonDivision = findViewById(R.id.button_division);
-        buttonComma = findViewById(R.id.button_comma);
-        buttonDot = findViewById(R.id.button_dot);
-        buttonEquals = findViewById(R.id.button_equals);
+    private fun initView() {
+        textView = findViewById(R.id.text_view)
+        buttonOne = findViewById(R.id.button_one)
+        buttonTwo = findViewById(R.id.button_two)
+        buttonThree = findViewById(R.id.button_three)
+        buttonFour = findViewById(R.id.button_four)
+        buttonFive = findViewById(R.id.button_five)
+        buttonSix = findViewById(R.id.button_six)
+        buttonSeven = findViewById(R.id.button_seven)
+        buttonEight = findViewById(R.id.button_eight)
+        buttonNine = findViewById(R.id.button_nine)
+        buttonZero = findViewById(R.id.button_zero)
+        buttonPlus = findViewById(R.id.button_plus)
+        buttonSubtract = findViewById(R.id.button_subtract)
+        buttonMultiply = findViewById(R.id.button_multiply)
+        buttonDivision = findViewById(R.id.button_division)
+        buttonComma = findViewById(R.id.button_comma)
+        buttonDot = findViewById(R.id.button_dot)
+        buttonEquals = findViewById(R.id.button_equals)
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button_one:
-                displayText.setText("1");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_two:
-                displayText.setText("2");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_three:
-                displayText.setText("3");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_four:
-                displayText.setText("4");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_five:
-                displayText.setText("5");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_six:
-                displayText.setText("6");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_seven:
-                displayText.setText("7");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_eight:
-                displayText.setText("8");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_nine:
-                displayText.setText("9");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_zero:
-                displayText.setText("0");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_plus:
-                displayText.setText("+");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_subtract:
-                displayText.setText("-");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_multiply:
-                displayText.setText("*");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_division:
-                displayText.setText("/");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_comma:
-                displayText.setText(",");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_dot:
-                displayText.setText(".");
-                textView.setText(displayText.getText());
-                break;
-            case R.id.button_equals:
-                displayText.setText("=");
-                textView.setText(displayText.getText());
-                break;
-            default:
-                break;
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_one -> {
+                displayText!!.text = "1"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_two -> {
+                displayText!!.text = "2"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_three -> {
+                displayText!!.text = "3"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_four -> {
+                displayText!!.text = "4"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_five -> {
+                displayText!!.text = "5"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_six -> {
+                displayText!!.text = "6"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_seven -> {
+                displayText!!.text = "7"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_eight -> {
+                displayText!!.text = "8"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_nine -> {
+                displayText!!.text = "9"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_zero -> {
+                displayText!!.text = "0"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_plus -> {
+                displayText!!.text = "+"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_subtract -> {
+                displayText!!.text = "-"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_multiply -> {
+                displayText!!.text = "*"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_division -> {
+                displayText!!.text = "/"
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_comma -> {
+                displayText!!.text = ","
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_dot -> {
+                displayText!!.text = "."
+                textView!!.text = displayText!!.text
+            }
+            R.id.button_equals -> {
+                displayText!!.text = "="
+                textView!!.text = displayText!!.text
+            }
+            else -> {}
         }
     }
 
-
+    companion object {
+        var TAG = "displayText"
+    }
 }
